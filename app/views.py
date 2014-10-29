750414593b9faf213c97667e5ac703520e466b6e
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-29 09:24:10 -0400

    Removing config settings for github deployment.

--- app/views.py
+++ app/views.py
@@ -21,21 +21,9 @@ def email_submissions():
         msg = Message(
             body=textwrap.dedent("""\
             You are receiving this email because you submitted your email
-            address at isyourwaterwater.com.\n
-            -------------------------\n
-            PRIMO TAKES THE RISK OUT OF WATER.\n
-            We believe a healthy lifestyle starts with the water you drink.
-            With high-quality Primo water in your home, there are no chances to
-             take -- you'll enjoy glass after glass of safe, refreshing,
-            healthy water.\n
-            We'd like to help you become a part of the Primo family. Just enter
-            the promo code FRACKNO at PrimoWater.com to get XX% off your
-            purchase of an innovative Primo dispenser.\n
-            http://somelink.com\n
-            -------------------------\n
-            isyourwaterwater.com"""),
-            subject='Thank You For Your Interest in Primo Water',
-            sender='noreply@isyourwaterwater.com',
+            address at someurl.com."""),
+            subject='Thank You For Your Interest!',
+            sender='noreply@someurl.com',
             recipients=[email])
         mail.send(msg)
     else:
--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_USERNAME = 'pbeeson@thevariable.com'
-    MAIL_PASSWORD = 'Six11BicycleC0'
+    MAIL_SERVER = ''
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    ADMINS = ['pbeeson@thevariable.com']
+    ADMINS = ['']
 
 
 class DevelopmentConfig(Config):
@@ -16,8 +16,8 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    MAIL_SERVER = 'smtp.webfaction.com'
-    MAIL_USERNAME = 'thevariable'
-    MAIL_PASSWORD = 'V@r1able'
+    MAIL_SERVER = ''
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
     MAIL_PORT = 465
     MAIL_USE_SSL = True
